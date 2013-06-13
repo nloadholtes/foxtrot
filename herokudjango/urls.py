202154cb842f0e1cb8fd42142dd09fdcb6a309f3
Nick Loadholtes <nick@ironboundsoftware.com>
2013-06-12 23:02:20 -0400

    Messing with the urls part 2

--- herokudjango/urls.py
+++ herokudjango/urls.py
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': ''}),
 )
 
