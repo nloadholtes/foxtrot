b0b60f28db6b8e703a75f19765af0851e5108504
Nick Loadholtes <nick@ironboundsoftware.com>
2013-06-23 22:25:37 -0400

    Forgot a field in the poll Adding in the collapable html class

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __unicode__(self):
         return self.question
