c8e099e59cae76877ceeb270c2b8dbe60d498941
Nick Loadholtes <nick@ironboundsoftware.com>
2013-06-23 22:32:45 -0400

    Finishing out the tutorial part 2

--- herokudjango/settings.py
+++ herokudjango/settings.py
@@ -107,10 +107,14 @@ ROOT_URLCONF = 'herokudjango.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'herokudjango.wsgi.application'
 
+import os.path
+PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    #'/path/to/mysite/templates', # Change this to your own directory.
+    os.path.join(PROJECT_DIR, "templates"),
 )
 
 INSTALLED_APPS = (
