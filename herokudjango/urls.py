0b3c0abec47d3817a85996948a2bff91681ce6c8
Nick Loadholtes <nick@ironboundsoftware.com>
2013-07-15 21:48:16 -0400

    Creating a new view in the personal api

--- herokudjango/urls.py
+++ herokudjango/urls.py
@@ -16,5 +16,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^personal_api/', 'personal_api.views.home')
 )
 
