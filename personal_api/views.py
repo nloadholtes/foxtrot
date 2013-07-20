e9761b26da699e057068bd95a2e9e080e1393c8c
Nick Loadholtes <nick@ironboundsoftware.com>
2013-07-20 12:45:26 -0400

    Starting to put in the header checks for github to prevent throttling

--- personal_api/views.py
+++ personal_api/views.py
@@ -4,7 +4,11 @@ import json
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 import requests
+import logging
 
+log = logging.getLogger()
+
+CACHE = {}
 
 class AboutView(TemplateView):
     template_name = "personal_api/about.html"
@@ -16,7 +20,18 @@ def getTweets(request):
 
 
 def getGitHubEvents(request, user):
-    print(user)
+    log.debug("User-> %s" % user)
+    #Do we have user info
+    userinfo = CACHE.get(user, {})
+    #Is it current
+    if len(userinfo) == 0:
+        print("Getting fresh user data")
+        requestdata = requests.get('https://api.github.com/users/%s/events' % user)
+        userinfo["etag"] = requestdata.headers["etag"]
+        output = requestdata.json()
+    #Is it a 304
+   
+
     output = requests.get('https://api.github.com/users/%s/events' % user).json()
 
     return HttpResponse(json.dumps(output), content_type="application/json")
