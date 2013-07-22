d03bc607208a4451ff9deb2753732e064b99feb5
Nick Loadholtes <nick@ironboundsoftware.com>
2013-07-21 20:53:33 -0400

    Cleanup

--- personal_api/views.py
+++ personal_api/views.py
@@ -10,6 +10,7 @@ log = logging.getLogger()
 
 CACHE = {}
 
+
 class AboutView(TemplateView):
     template_name = "personal_api/about.html"
 
@@ -18,27 +19,27 @@ def getTweets(request):
     output = {}
     return HttpResponse(json.dumps(output), content_type="application/json")
 
+
 def _setUserInfo(userinfo, requestdata):
     userinfo["etag"] = requestdata.headers["etag"]
     userinfo["output"] = requestdata.json()
 
+
 def getGitHubEvents(request, user):
     log.debug("User-> %s" % user)
-    #Do we have user info
     userinfo = CACHE.get(user, {})
-    #Is it current
+    #Update user cache if it isn't there
     if len(userinfo) == 0:
         print("Getting fresh user data")
         requestdata = requests.get('https://api.github.com/users/%s/events' % user)
         _setUserInfo(userinfo, requestdata)
 
-    #Is it a 304
+    #Github has requested that everyone honor the etag
     headers = {"If-None-Match": userinfo["etag"]}
     requestdata = requests.get('https://api.github.com/users/%s/events' % user, headers=headers)
     if requestdata.status_code != 304:
         _setUserInfo(userinfo, requestdata)
 
     output = userinfo["output"]
-    #requests.get('https://api.github.com/users/%s/events' % user).json()
 
     return HttpResponse(json.dumps(output), content_type="application/json")
