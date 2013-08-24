177086c35e58dfd68a5a660f946beb272541906d
Nick Loadholtes <nick@ironboundsoftware.com>
2013-08-24 16:44:58 -0400

    Twitter data now feeding in

--- personal_api/views.py
+++ personal_api/views.py
@@ -5,9 +5,8 @@ import os
 
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-import oauth2 as oauth
 import requests
-
+from requests_oauthlib import OAuth1Session
 
 log = logging.getLogger()
 
@@ -18,30 +17,14 @@ class AboutView(TemplateView):
     template_name = "personal_api/about.html"
 
 
-def oauth_req(url, key, secret, http_method="GET", post_body=None,
-        http_headers=None):
-    consumer = oauth.Consumer(key=os.environ["CONSUMER_KEY"], secret=os.environ["CONSUMER_SECRET"])
-    token = oauth.Token(key=key, secret=secret)
-    client = oauth.Client(consumer, token)
-
-    resp, content = client.request(
-        url,
-        method=http_method,
-        body=post_body,
-        headers=http_headers,
-    )
-    return content
-
-
 def getTweets(request):
-    home_timeline = oauth_req(
-        'https://api.twitter.com/1.1/statuses/home_timeline.json',
-        os.environ['ACCESS_TOKEN'],
-        os.environ['ACCESS_SECRET'],
-        post_body="Nothing to see here"
-    )
-    # url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-    return HttpResponse(json.dumps(home_timeline), content_type="application/json")
+    twitter = OAuth1Session(resource_owner_key=os.environ['ACCESS_TOKEN'],
+                            resource_owner_secret=os.environ['ACCESS_SECRET'],
+                            client_key=os.environ['CONSUMER_KEY'],
+                            client_secret=os.environ['CONSUMER_SECRET'],)
+    url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+    r = twitter.get(url)
+    return HttpResponse(r.content, content_type="application/json")
 
 
 def _setUserInfo(userinfo, requestdata):
